39778683c4f0dcd1b2fed0722f58ca3c4a46880b
𝐒𝐇𝐄𝐈𝐊𝐇 𝐀𝐋𝐈 𝐀𝐇𝐌𝐀𝐃 <aliahmad241224@gmail.com>
2025-04-19 21:10:50 +0500

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -11,7 +11,7 @@ cmd({
     filename: __filename,
 },
 async (conn, mek, m, { from, reply }) => {
-    const githubRepoURL = 'https://github.com/Sheikh-ali-2412/SHEIKH-ALI-MD';
+    const githubRepoURL = 'https://github.com/SHEIKH-ALI-2402/SHEIKH-ALI-MD';
 
     try {
         const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/);
@@ -188,7 +188,7 @@ ${repoData.description || 'No description provided'}
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/XdTechPro/KHAN-DATA/raw/refs/heads/main/autovoice/menunew.m4a' },
+            audio: { url: 'https://github.com/SHEIKH-ALI-2402/SHEIKH-ALI-MD/raw/refs/heads/main/autovoice/menunew.m4a' },
             mimetype: 'audio/mp4',
             ptt: true,
             contextInfo: { 
