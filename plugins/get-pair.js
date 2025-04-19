b3c4944a7391644bf0a783f2861003d490f1465a
𝐒𝐇𝐄𝐈𝐊𝐇 𝐀𝐋𝐈 𝐀𝐇𝐌𝐀𝐃 <aliahmad241224@gmail.com>
2025-04-19 15:47:04 +0500

    Update get-pair.js

--- plugins/get-pair.js
+++ plugins/get-pair.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
 
         // Fetch pairing code
         //const fetch = require("node-fetch");
-        const response = await fetch(`https://sheikh-ali-pair-2-7c08b07b7cab.herokuapp.com/pair?phone=${q}`);
+        const response = await fetch(`https://sheikh-ali-md-pair.onrender.com/pair?phone=${q}`);
         const pair = await response.json();
 
         // Check for errors in response
