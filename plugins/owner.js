7b3ea8cddda3ff489af0afc5561f1bf09b652c8a
𝐒𝐇𝐄𝐈𝐊𝐇 𝐀𝐋𝐈 𝐀𝐇𝐌𝐀𝐃 <aliahmad241224@gmail.com>
2025-04-20 18:45:17 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -29,7 +29,7 @@ async (conn, mek, m, { from }) => {
 
         // Send the owner contact message with image and audio
         await conn.sendMessage(from, {
-            image: { url: 'https://files.catbox.moe/149k8x.jpg' }, // Image URL from your request
+            image: { url: 'https://i.ibb.co/xqkj0Jkr/9557.jpg' }, // Image URL from your request
             caption: `╭━━〔 *SHEIKH-ALI-MD* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *Name* - ${ownerName}
